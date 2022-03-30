ed99ada1f30ff22a343f760b2ccd1c28b1049847
Chris P Varghese <69855601+EAGLERUSHER@users.noreply.github.com>
2022-03-30 22:39:04 +0530

    Login page auth

--- reg.py
+++ reg.py
@@ -26,14 +26,38 @@ def home():
         email = request.form['email']
         password = request.form['password']
         
+        cursor.execute('SELECT * FROM users WHERE emailaddress = ? AND password = ?', (email, password, ))
+        account = cursor.fetchone()
+        if account:
+            # session['loggedin'] = True
+            session['email'] = account[1]
+            session['password'] = account[2]
+            msg = 'Logged in successfully !'
+            print(msg)
+            return render_template('res.html', msg = msg)
+        else:
+            msg = 'Incorrect username / password !'
+            print(msg)
+            return render_template('error.html', msg = msg)
+    return render_template('index.html', msg = msg)
+
+# --------------------------------------------------------------------------------
+    
+    connection =sqlite3.connect("user1.db")
+    cursor = connection.cursor()
+    msg = ''
+    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
+        email = request.form['email']
+        password = request.form['password']
+        
         cursor.execute('SELECT * FROM users WHERE emailaddress = ? AND password = ?', (email, password))
         account = cursor.fetchone()
-        print(account)
+        
         if account:
             # session['loggedin'] = True
             
             session['email'] = account[1]
-            session['password'] = account[3]
+            session['password'] = account[2]
             msg = 'Logged in successfully !'
             print(msg)
             return render_template('res.html', msg = msg)
